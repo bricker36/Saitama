c1aefdddf26e1faccd5abbe679ce0f3e4b8dd3af
bricker36 <34772047+bricker36@users.noreply.github.com>
2017-12-22 18:32:31 -0500

    search
    created search function

---
+++ script/js/search.js
@@ -0,0 +1,41 @@
+function search(query){
+  switch (query.substr(0, 2)) {
+
+    // Wikipedia
+    case "w:":
+      query = query.substr(2);
+      window.location = "https://en.wikipedia.org/wiki/Special:Search/" + query;
+      break;
+
+    // Amazon - Smile
+    case "a:":
+        query = query.substr(2);
+        window.location = "https://smile.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=" + query;
+        break;
+
+    // YouTube
+    case "y:":
+        query = query.substr(2);
+        window.location = "https://www.youtube.com/results?search_query=" + query;
+        break;
+
+    // Default - Google
+    default:
+      window.location = "https://www.google.com/search?q=" + query;
+
+  }
+}
+window.onload = function(){
+  //search
+
+  searchinput = document.getElementById("searchbox");
+  if (!!searchinput) {
+    searchinput.addEventListener("keypress", function(a){
+      var key = a.keyCode;
+      if(key == 13){
+        var query = this.value;
+        search(query);
+      }
+    })
+  }
+}
