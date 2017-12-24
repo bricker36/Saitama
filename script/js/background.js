f7ae6b3415a74486420d2fa305bd4123fcf2d847
bricker36 <34772047+bricker36@users.noreply.github.com>
2017-12-24 04:19:31 -0500

    billboard img change
    fixed to work with mouseenter/mouseleave

--- script/js/background.js
+++ script/js/background.js
@@ -1,39 +1,44 @@
-$("#button-1").hover(function() {
+$("#nav").mouseleave(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-1').toggleClass("bb-transparent");
+  $('#bb-0').removeClass("bb-transparent");
 })
 
-$("#button-2").hover(function() {
+$("#menu-1").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-2').toggleClass("bb-transparent");
+  $('#bb-1').removeClass("bb-transparent");
 })
 
-$("#button-3").hover(function() {
+$("#menu-2").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-3').toggleClass("bb-transparent");
+  $('#bb-2').removeClass("bb-transparent");
 })
 
-$("#button-4").hover(function() {
+$("#menu-3").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-4').toggleClass("bb-transparent");
+  $('#bb-3').removeClass("bb-transparent");
 })
 
-$("#button-5").hover(function() {
+$("#menu-4").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-5').toggleClass("bb-transparent");
+  $('#bb-4').removeClass("bb-transparent");
 })
 
-$("#button-6").hover(function() {
+$("#menu-5").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-6').toggleClass("bb-transparent");
+  $('#bb-5').removeClass("bb-transparent");
 })
 
-$("#button-7").hover(function() {
+$("#menu-6").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-7').toggleClass("bb-transparent");
+  $('#bb-6').removeClass("bb-transparent");
 })
 
-$("#button-8").hover(function() {
+$("#menu-7").mouseenter(function() {
   $('#billboard img').addClass("bb-transparent");
-  $('#bb-8').toggleClass("bb-transparent");
+  $('#bb-7').removeClass("bb-transparent");
+})
+
+$("#menu-8").mouseenter(function() {
+  $('#billboard img').addClass("bb-transparent");
+  $('#bb-8').removeClass("bb-transparent");
 })
