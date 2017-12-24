464bbeb01b9a06788710dfb8870111b0f7817d20
bricker36 <34772047+bricker36@users.noreply.github.com>
2017-12-24 02:21:26 -0500

    billboard
    small tweaks
    click -> hover
    different imgs

--- script/js/background.js
+++ script/js/background.js
@@ -1,31 +1,39 @@
-$("#button-1").click(function() {
+$("#button-1").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-1').toggleClass("bb-transparent");
 })
 
-$("#button-2").click(function() {
+$("#button-2").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-2').toggleClass("bb-transparent");
 })
 
-$("#button-3").click(function() {
+$("#button-3").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-3').toggleClass("bb-transparent");
 })
 
-$("#button-4").click(function() {
+$("#button-4").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-4').toggleClass("bb-transparent");
 })
 
-$("#button-5").click(function() {
+$("#button-5").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-5').toggleClass("bb-transparent");
 })
 
-$("#button-6").click(function() {
+$("#button-6").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-6').toggleClass("bb-transparent");
 })
 
-$("#button-7").click(function() {
+$("#button-7").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-7').toggleClass("bb-transparent");
 })
 
-$("#button-8").click(function() {
+$("#button-8").hover(function() {
+  $('#billboard img').addClass("bb-transparent");
   $('#bb-8').toggleClass("bb-transparent");
 })
